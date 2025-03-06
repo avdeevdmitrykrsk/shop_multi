18acefd17ded5f7b918fedc1f4e09ecde0a83e7f
avdeevdmitrykrsk <153370353+avdeevdmitrykrsk@users.noreply.github.com>
2025-03-06 22:43:12 +0700

    Добавил автоматическое создание спецификаций swagger.

--- backend/users/signals.py
+++ backend/users/signals.py
@@ -9,10 +9,15 @@ User = get_user_model()
 @receiver(post_migrate)
 def create_superuser(sender, **kwargs):
     if sender.name == 'users':
-        if not User.objects.filter(email='edu-avdeev@ya.ru').exists():
-            User.objects.create_superuser(
-                username='homie',
-                email='edu-avdeev@ya.ru',
-                password='Zb0dd445',
-                phone_number=9232982225,
-            )
+        User.objects.create_superuser(
+            username='homie',
+            email='edu-avdeev@ya.ru',
+            password='Zb0dd445',
+            phone_number=9232982225,
+        )
+        User.objects.create_user(
+            username='homie_just_a_user',
+            email='user@ya.ru',
+            password='Zb0dd445',
+            phone_number=9232982226,
+        )
