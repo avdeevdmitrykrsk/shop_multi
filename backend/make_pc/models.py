33060ca8dc1d099f62000bb58f80a15d1ea9aa77
avdeevdmitrykrsk <153370353+avdeevdmitrykrsk@users.noreply.github.com>
2025-03-20 02:11:34 +0700

    Рефакторинг

--- backend/make_pc/models.py
+++ backend/make_pc/models.py
@@ -1,99 +1,41 @@
 # Thirdparty imports
-from django.core.cache import cache
 from django.db import models
 
 # Projects imports
+from make_pc.constants import PC_DIY_QUERY_STMNT
 from products.models import Product, ProductProperty
 
 
 class PcDIYManager(models.Manager):
 
     def get_annotated_queryset(self, user):
-        product_property_queryset = cache.get('product_property_queryset')
-        if not product_property_queryset:
-            product_property_queryset = ProductProperty.objects.select_related(
-                'property'
+        product_property_queryset = ProductProperty.objects.select_related(
+            'property'
+        )
+
+        prefetch_fields = [
+            'pc_box',
+            'power_supply',
+            'motherboard',
+            'ram_memory',
+            'ssd_storage_memory',
+            'hdd_storage_memory',
+            'cpu',
+            'gpu',
+        ]
+
+        prefetches = [
+            models.Prefetch(
+                f'{field}__product_property_prod',
+                queryset=product_property_queryset,
             )
-            cache.set(
-                'product_property_queryset', product_property_queryset, 10
-            )
+            for field in prefetch_fields
+        ]
 
         queryset = super().get_queryset()
 
-        return queryset.select_related(
-            'pc_box__creator',
-            'pc_box__category',
-            'pc_box__sub_category',
-            'pc_box__article_by_product',
-            'pc_box__product_type',
-            'power_supply__creator',
-            'power_supply__category',
-            'power_supply__sub_category',
-            'power_supply__article_by_product',
-            'power_supply__product_type',
-            'motherboard__creator',
-            'motherboard__category',
-            'motherboard__sub_category',
-            'motherboard__article_by_product',
-            'motherboard__product_type',
-            'ram_memory__creator',
-            'ram_memory__category',
-            'ram_memory__sub_category',
-            'ram_memory__article_by_product',
-            'ram_memory__product_type',
-            'ssd_storage_memory__creator',
-            'ssd_storage_memory__category',
-            'ssd_storage_memory__sub_category',
-            'ssd_storage_memory__article_by_product',
-            'ssd_storage_memory__product_type',
-            'hdd_storage_memory__creator',
-            'hdd_storage_memory__category',
-            'hdd_storage_memory__sub_category',
-            'hdd_storage_memory__article_by_product',
-            'hdd_storage_memory__product_type',
-            'cpu__creator',
-            'cpu__category',
-            'cpu__sub_category',
-            'cpu__article_by_product',
-            'cpu__product_type',
-            'gpu__creator',
-            'gpu__category',
-            'gpu__sub_category',
-            'gpu__article_by_product',
-            'gpu__product_type',
-        ).prefetch_related(
-            models.Prefetch(
-                'pc_box__product_property_prod',
-                queryset=product_property_queryset,
-            ),
-            models.Prefetch(
-                'power_supply__product_property_prod',
-                queryset=product_property_queryset,
-            ),
-            models.Prefetch(
-                'motherboard__product_property_prod',
-                queryset=product_property_queryset,
-            ),
-            models.Prefetch(
-                'ram_memory__product_property_prod',
-                queryset=product_property_queryset,
-            ),
-            models.Prefetch(
-                'ssd_storage_memory__product_property_prod',
-                queryset=product_property_queryset,
-            ),
-            models.Prefetch(
-                'hdd_storage_memory__product_property_prod',
-                queryset=product_property_queryset,
-            ),
-            models.Prefetch(
-                'cpu__product_property_prod',
-                queryset=product_property_queryset,
-            ),
-            models.Prefetch(
-                'gpu__product_property_prod',
-                queryset=product_property_queryset,
-            ),
+        return queryset.select_related(*PC_DIY_QUERY_STMNT).prefetch_related(
+            *prefetches
         )
 
 
